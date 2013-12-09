b539ad5fa82678c178d8c6ed129f0e83f83e1f71
Gabor Javorszky <gabor@javorszky.co.uk>
2013-12-09 08:17:28 +0000

    Working demo of image grid

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php 
+
+if( !function_exists('es_preit') ) {
+    function es_preit( $obj, $echo = true ) {
+        if( $echo ) {
+            echo '<pre>';
+            print_r( $obj );
+            echo '</pre>';
+        } else {
+            return '<pre>' . print_r( $obj, true ) . '</pre>';
+        }
+    }
+}
+
+if( !function_exists('es_silent') ) {
+    function es_silent( $obj ) {
+          ?>
+        <div style="display: none">
+            <pre><?php print_r( $obj ); ?></pre>
+        </div>
+        <?php
+    }
+}
+
+
+ ?><!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.7.1.min.js"></script>
+        <script src="js/vendor/underscore.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <h1>This is where the images go</h1>
+        <ul class="container clearfix">
+            <?php 
+            $files = glob('img/*.jpg', GLOB_NOSORT);
+            // es_preit($files);
+            foreach ($files as $file) {
+                echo "<li><img src='{$file}'></li>";
+                
+            }
+            
+            ?>
+            
+        </ul>
+    
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="jg.jq.griddify.js"></script>
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,67 @@
+(function ($) {
+	$(window).load(function () {
+		var container = $('.container'),
+			images = container.find('img'),
+			imgLoaded = 0,
+			imgTotal = images.length,
+			containerWidth = container.width(),
+			adjust = function (distinctRows) {
+				_.each(distinctRows, function (element, index, list) {
+                    console.log(index, list.length);
+					if( index === list.length-1) {
+						console.log(index);
+						return;
+					}
+					var adjustSize = element.width - containerWidth,
+						margin = parseInt(adjustSize / (element.images.length*2)),
+						marginLast = (adjustSize - (element.images.length*2*margin)) + margin;
+
+					_.each(element.images, function (element, index, list) {
+						var _width;
+						if (index === list.length-1) {
+							_width = element.width() - margin - marginLast;
+							element.css({
+								'margin-left': -margin,
+								'margin-right': -marginLast
+							});
+
+							element.parent().width(_width);
+						} else {
+							_width = element.width() - 2*margin;
+							element.css({
+								'margin-left': -margin,
+								'margin-right': -margin
+							});
+							element.parent().width(_width);
+						}
+					});
+
+				});
+			}
+			getRows = function () {
+				var distinctRows = [],
+					rowIndex = 0,
+					_width = 0;
+				_.each(images, function (element, index) {
+					distinctRows[rowIndex] = distinctRows[rowIndex] || [];
+					distinctRows[rowIndex]['images'] = distinctRows[rowIndex]['images'] || [];
+					distinctRows[rowIndex]['width'] = distinctRows[rowIndex]['width'] || 0;
+					var $el = $(element);
+					_width += $el.width()+10;
+					// console.log(rowIndex);
+					// console.log($el);
+					if(_width < containerWidth) {
+						distinctRows[rowIndex]['images'].push($el);
+					} else {
+						distinctRows[rowIndex]['images'].push($el);
+						distinctRows[rowIndex]['width'] = _width;
+						_width = 0;
+						rowIndex += 1;
+					}
+
+				});
+				adjust(distinctRows);
+			},
+			rows = getRows();
+	});
+}(jQuery));
